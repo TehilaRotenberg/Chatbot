8131cffc0f4eb16841e1387754013a6c9c01f5be
יוסי היימן <hayossi@gmail.com>
2022-04-27 20:04:20 +0300

    making plug 12- reverse sentence

---
+++ ReverseSentencePlugin/Reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasePlugin;
+using BasePlugin.Interfaces;
+using BasePlugin.Records;
+
+namespace ReverseSentencePlugin
+{
+    public class Reverse: IPlugin
+    {
+
+        public static string _Id = "reversed";
+        public string Id => _Id;
+        public PluginOutput Execute(PluginInput input)
+        {
+                return new PluginOutput(input.Message.Reverse().ToString(),input.PersistentData);
+        }
+    }
+}
