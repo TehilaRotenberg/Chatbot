532f8cab4c8829c02f7424da707dbec94a526462
TehilaRotenberg <89943199+TehilaRotenberg@users.noreply.github.com>
2022-05-15 19:29:07 +0300

    Addition and subtraction up to 20

---
+++ AddAndSub/AddAndSub.cs
@@ -0,0 +1,20 @@
+﻿using BasePlugin.Interfaces;
+using BasePlugin.Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddAndSub
+{
+    public class AddAndSub : IPlugin
+    {
+        public string Id => throw new NotImplementedException();
+
+        public PluginOutput Execute(PluginInput input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
